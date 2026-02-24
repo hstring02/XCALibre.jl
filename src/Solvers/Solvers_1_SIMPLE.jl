abc3177ea8e54ec5397ecb19ccfbb4802695d8cc
hstring02 <harrystringerwork@gmail.com>
2026-02-24 15:36:15 +0000

    Small mods

--- src/Solvers/Solvers_1_SIMPLE.jl
+++ src/Solvers/Solvers_1_SIMPLE.jl
@@ -267,7 +267,9 @@ function SIMPLE(
             @info "Simulation converged in $iteration iterations!"
             if !signbit(write_interval)
                 make_absolute_velocity!(U,  x0, rotaxis, omega, config)
-                save_output(model, outputWriter, iteration, time, config)
+                #save_output(model, outputWriter, iteration, time, config)
+                save_output_polar(model, outputWriter, iteration, time, config, x0, rotaxis
+    )
             end
             break
         end
@@ -287,7 +289,7 @@ function SIMPLE(
         
         if iteration%write_interval + signbit(write_interval) == 0      
             # make_absolute_velocity!(U,  x0, rotaxis, omega, config)
-            save_output(model, outputWriter, iteration, time, config)
+            # save_output(model, outputWriter, iteration, time, config)
             save_postprocessing(postprocess,iteration,time,mesh,outputWriter,config.boundaries)
         end
 
@@ -453,4 +455,27 @@ end
     Omega = omega*rotaxis
     r = cells[cID].centre - x0
     U[cID] = U[cID] + Omega × r
+end
+
+function save_output_polar(model::Physics{T,F,SO,M,Tu,E,D,BI}, outputWriter, iteration, time, config, x0, rotaxis
+    ) where {T,F,SO,M,Tu,E,D,BI}
+    U = model.momentum.U
+    mesh = U.mesh
+    cells = mesh.cells
+    Up = VectorField(mesh)
+
+    for i ∈ eachindex(Up.x.values)
+        r = cells[i].centre - x0
+        r_norm = r./norm(r)
+        tang = r_norm × rotaxis
+        Up.x.values[i] = U[i] ⋅ r_norm
+        Up.z.values[i] = U.z.values[i]
+        Up.y.values[i] = U[i] ⋅ tang
+    end
+    args = (
+        ("U", model.momentum.U), 
+        ("Up", Up), 
+        ("p", model.momentum.p)
+    )
+    write_results(iteration, time, model.domain, outputWriter, config.boundaries, args...)
 end
