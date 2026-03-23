924c631658db4b5fc9c30a8e1500c43f66c9bf8e
hstring02 <harrystringerwork@gmail.com>
2026-03-23 10:07:32 +0000

    Last commit :(

--- src/ReferenceFrames/1_MRF_zone_maker.jl
+++ src/ReferenceFrames/1_MRF_zone_maker.jl
@@ -1,21 +1,23 @@
 export radial_mask!
 
-function radial_mask!(x0, radius, mask, hardware, mesh)
+function radial_mask!(x0, radius_inner, radius_outer, mask, hardware, mesh)
     (; backend, workgroup) = hardware
     cells = mesh.cells 
 
     ndrange = length(cells)
     kernel! = _radial_mask!(_setup(backend, workgroup, ndrange)...)
-    kernel!(x0, radius, mask, cells)
+    kernel!(x0, radius_inner, radius_outer, mask, cells)
 end
 
-@kernel function _radial_mask!(x0, radius, mask, cells)
+@kernel function _radial_mask!(x0, radius_inner, radius_outer, mask, cells)
     cID = @index(Global)
 
     r = cells[cID].centre - x0
     length = norm(r)
-    if length <= radius
-        mask[cID] = 1
+    if length <= radius_outer
+        if length >= radius_inner
+            mask[cID] = 1
+        end
     end
 
 end
